f45214b55cf49a79c1b2b36fae4b3d1c220e4402
layzonb <layzon@gmail.com>
2021-04-04 00:08:12 -0600

    lab

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -1,24 +1,48 @@
+import random
 
 # Soldier
-
-
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
 
 # Viking
+class Viking(Soldier):
+    
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
 
+    def receiveDamage(self, damage):
+        self.health -= damage
 
-class Viking:
-    pass
+        if self.health > 0:
+            return f'{self.name} has received {damage} points of damage'
+
+        elif self.health <= 0:
+            return f'{self.name} has died in act of combat'
+
+    def battleCry(self):
+        return 'Odin Owns You All!'
 
 # Saxon
+class Saxon(Soldier):
+    
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
 
+    def receiveDamage(self, damage):
+        self.health -= damage
 
-class Saxon:
-    pass
+        if self.health > 0:
+            return f'A Saxon has received {damage} points of damage'
 
-# War
+        elif self.health <= 0:
+            return f'A Saxon has died in combat'
 
-
-class War:
-    pass
